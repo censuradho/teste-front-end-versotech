a44666b5c8d470ee5b4bb39190bcb8a8816cd0da
censuradho <gustavoleiteoliveira800@gmail.com>
2023-04-12 10:17:21 -0300

    refactor(ssr): inferring query params on getServerSideProps request

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,20 @@ import { PokemonPageProps } from "@/layout/pokemon/types"
 import { pokemonService } from "@/services/pokeapi/pokemon"
 import { GetServerSideProps, InferGetServerSidePropsType } from "next"
 
-export const getServerSideProps: GetServerSideProps<PokemonPageProps> = async () => {
-  const data = await pokemonService.findMany()
+export const getServerSideProps: GetServerSideProps<PokemonPageProps> = async (context) => {
+  const { 
+    page = 1,
+    limit,
+    search
+  } = context.query
+
+  const _page = Number(page) || 1
+
+  const data = await pokemonService.findMany({
+    page: _page,
+    search: search as string,
+    ...(limit && ({ limit: Number(limit )})) 
+  })
 
   return {
     props: {
